858c96e18bd64589f3621544757e8abd03f5af23
devin_mason <mvp@vastofi.com>
2025-08-08 15:24:20 -0400

    Finalize check on last commit

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
@@ -172,4 +174,4 @@ export const DropdownMenuItem = React.forwardRef<HTMLDivElement, DropdownMenuIte
   }
 );
 
-DropdownMenuItem.displayName = "DropdownMenuItem"; 
+DropdownMenuItem.displayName = "DropdownMenuItem";
