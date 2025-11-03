122de5b828adb73be9f54e8005fd9c537cd059fe
devin_mason <mvp@vastofi.com>
2025-11-03 09:31:26 -0500

    FIX: Enforcing stricter csp for possible crash fix

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self';",
-      "script-src 'self' https://www.googletagmanager.com;",
-      "style-src 'self' https://fonts.googleapis.com;",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
       "img-src 'self' data: https:;",
       "font-src 'self' https://fonts.gstatic.com;",
       "connect-src 'self' https://ssqpfkiesxwnmphwyezb.supabase.co https://*.supabase.co https://*.supabase.in;",
