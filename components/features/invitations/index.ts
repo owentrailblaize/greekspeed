ec620ba348a83131d0c1ec9d553776002de60022
devin_mason <dmasonone@gmail.com>
2026-01-19 15:51:26 -0500

    Initial cleanup of codebase unused files

--- components/features/invitations/index.ts
+++
@@ -1,5 +0,0 @@
-export { InviteManagement } from './InviteManagement';
-export { CreateInviteModal } from './CreateInviteModal';
-export { InviteSettings } from './InviteSettings';
-export { InviteList } from './InviteList';
-
--- components/features/messaging/index.ts
+++
@@ -1,4 +0,0 @@
-export { MessageList } from './MessageList';
-export { MessageInput } from './MessageInput';
-export { ChatWindow } from './ChatWindow';
-export { ConnectionChat } from './ConnectionChat'; 
